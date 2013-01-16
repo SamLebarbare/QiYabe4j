f2ea992e7809c1029ffaad71df0ea359cbfad4f5
Samuel Loup <samuel.loup@gmail.com>
2013-01-16 23:33:59 +0100

    First simple model iteration

---
+++ src/main/java/org/qi4j/sample/qiyabe4j/app/domain/Account.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2013, Paul Merlin. All Rights Reserved.
+ * Copyright (c) 2013, Samuel Loup. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.qi4j.sample.qiyabe4j.app.domain;
+
+import org.qi4j.api.association.ManyAssociation;
+import org.qi4j.api.entity.EntityComposite;
+import org.qi4j.api.mixin.Mixins;
+import org.qi4j.api.property.Property;
+
+@Mixins( Account.Mixin.class )
+public interface Account extends EntityComposite {
+    
+    Property<String> displayName();
+    
+    Property<String> login();
+    
+    Property<String> password();
+    
+    Property<String> email();
+    
+    ManyAssociation<Post> posts();
+    
+    ManyAssociation<Comment> comments();
+    
+    abstract class Mixin implements Account {
+        
+    }
+    
+    
+    
+}
